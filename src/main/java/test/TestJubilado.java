c99fc3b9bd601ee23b41f300d56699b67c7c76a0
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-08-06 15:14:03 -0500

    v1

--- src/main/java/test/TestJubilado.java
+++ src/main/java/test/TestJubilado.java
@@ -2,14 +2,23 @@
 package test;
 
 import dominio.*;
+import java.util.ArrayList;
 
 public class TestJubilado {
     public static void main(String[] args) {
         
-        System.out.println("Jubilados");
-        Jubilado[] jubilados = new Jubilado[5];
-        jubilados[0] = new Jubilado ("");
-        jubilados[0].calcularSalario();
+        ArrayList<Jubilado> jubilados = new ArrayList<>();
+        
+        JubilacionVejez p1 = new JubilacionVejez ("1234567891","Sofia Moncayo",700,15);
+        JubilacionDiscapacidad p2 = new JubilacionDiscapacidad("2345678911","Luz Moreno",800,30,35);
+        JubilacionPatronal p3 = new JubilacionPatronal("2345678911","Nicolás Díaz",1200,40,4.28);
+        jubilados.add(p1);
+        jubilados.add(p2);
+        jubilados.add(p3);
+        
+        for(int i =0; i<jubilados.size();i++){
+            System.out.println(jubilados);
+        }
         
         
     }
